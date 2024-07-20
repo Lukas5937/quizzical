a7eded10f91baaec1cad3b12291c98dcb370eaf4
Lukas5937 <lukas-reich@t-online.de>
2024-07-20 09:13:06 +0200

    Add error handling to createHighScoreEntry

--- backend/models/highscore-model.js
+++ backend/models/highscore-model.js
@@ -1,14 +1,17 @@
 import mongoose from 'mongoose'
 
-const highScoreDataSchema = new mongoose.Schema({
+const Schema = mongoose.Schema
+
+const highScoreEntrySchema = new Schema({
   userName: { type: String, required: true },
   correctAnswers: { type: Number, required: true },
   duration: { type: Number, required: true },
   category: { type: String, required: true },
   difficulty: { type: String, required: true },
+  date: { type: String, required: true },
 })
 
-export const HighScoreData = mongoose.model(
-  'highScoreData',
-  highScoreDataSchema
+export const HighScoreEntry = mongoose.model(
+  'highScoreEntry',
+  highScoreEntrySchema
 )
--- backend/mongoose.js
+++ backend/mongoose.js
@@ -1,13 +1,22 @@
-import { HighScoreData } from './models/highscore-model'
+import { HighScoreEntry } from './models/highscore-model'
+import HttpError from './models/http-error'
 
 export const createHighScoreEntry = async (req, res, next) => {
-  const highScoreEntry = new HighScoreData({
+  const createdHighScoreEntry = new HighScoreEntry({
     userName: req.body.userName,
     correctAnswers: req.body.correctAnswers,
     duration: req.body.duration,
     category: req.body.category,
     difficulty: req.body.difficulty,
   })
-  const result = await highScoreEntry.save()
-  res.json(result)
+  try {
+    await createdHighScoreEntry.save()
+  } catch (err) {
+    const error = new HttpError(
+      'Uploading quiz results to high scores failed, please try again.',
+      500
+    )
+    return next(error)
+  }
+  res.status(201).json(createdHighScoreEntry)
 }
