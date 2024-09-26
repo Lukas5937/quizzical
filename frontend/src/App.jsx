93de38671034123f26e0e2b103a07ae69eb9049b
Lukas5937 <lukas-reich@t-online.de>
2024-09-26 08:22:57 +0200

    Add lazy loading to frontend routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,28 @@
+import { lazy, Suspense } from 'react'
 import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom'
 import ContextProviders from './context/ContextProviders.jsx'
 import Navigation from './Navigation.jsx'
 import ErrorPage from './ErrorPage.jsx'
 import Home from './Home.jsx'
 import QuizSettings from './quiz/pages/QuizSettings'
-import QuizGame from './quiz/pages/QuizGame'
-import QuizResults from './quiz/pages/QuizResults'
-import HighScoresNavigation from './highscores/components/HighScoresNavigation.jsx'
-import HighScoresDataEasy from './highscores/pages/HighScoresDataEasy.jsx'
-import HighScoresDataMedium from './highscores/pages/HighScoresDataMedium.jsx'
-import HighScoresDataHard from './highscores/pages/HighScoresDataHard.jsx'
+// import QuizGame from './quiz/pages/QuizGame'
+// import QuizResults from './quiz/pages/QuizResults'
+import HighScoresNavigation from './high-scores/components/HighScoresNavigation.jsx'
+import CircularProgress from '@mui/material/CircularProgress'
+
+const QuizGame = lazy(() => import('./quiz/pages/QuizGame'))
+
+const QuizResults = lazy(() => import('./quiz/pages/QuizResults'))
+
+const HighScoresDataEasy = lazy(() =>
+  import('./high-scores/pages/HighScoresDataEasy.jsx')
+)
+const HighScoresDataMedium = lazy(() =>
+  import('./high-scores/pages/HighScoresDataMedium.jsx')
+)
+const HighScoresDataHard = lazy(() =>
+  import('./high-scores/pages/HighScoresDataHard.jsx')
+)
 
 function App() {
   const router = createBrowserRouter([
@@ -20,16 +33,71 @@ function App() {
       children: [
         { path: '', element: <Home /> },
         { path: 'quiz/settings', element: <QuizSettings /> },
-        { path: 'quiz/game', element: <QuizGame /> },
-        { path: 'quiz/results', element: <QuizResults /> },
+        {
+          path: 'quiz/game',
+          element: (
+            <Suspense
+              fallback={
+                <CircularProgress className="circular-progress center" />
+              }
+            >
+              <QuizGame />
+            </Suspense>
+          ),
+        },
+        {
+          path: 'quiz/results',
+          element: (
+            <Suspense
+              fallback={
+                <CircularProgress className="circular-progress center" />
+              }
+            >
+              <QuizResults />
+            </Suspense>
+          ),
+        },
         {
           path: 'high-scores',
           element: <HighScoresNavigation />,
           children: [
             { path: '', element: <Navigate to="hard" replace /> },
-            { path: 'easy', element: <HighScoresDataEasy /> },
-            { path: 'medium', element: <HighScoresDataMedium /> },
-            { path: 'hard', element: <HighScoresDataHard /> },
+            {
+              path: 'easy',
+              element: (
+                <Suspense
+                  fallback={
+                    <CircularProgress className="circular-progress center" />
+                  }
+                >
+                  <HighScoresDataEasy />
+                </Suspense>
+              ),
+            },
+            {
+              path: 'medium',
+              element: (
+                <Suspense
+                  fallback={
+                    <CircularProgress className="circular-progress center" />
+                  }
+                >
+                  <HighScoresDataMedium />
+                </Suspense>
+              ),
+            },
+            {
+              path: 'hard',
+              element: (
+                <Suspense
+                  fallback={
+                    <CircularProgress className="circular-progress center" />
+                  }
+                >
+                  <HighScoresDataHard />
+                </Suspense>
+              ),
+            },
           ],
         },
         { path: '*', element: <Navigate to="/" replace /> },
